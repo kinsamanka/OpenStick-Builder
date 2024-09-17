28914bbec51966d98a40c3d4749a16fd6c4e7e70
GP Orcullo <kinsamanka@gmail.com>
2024-09-17 16:13:57 +0800

    fix recovery button on UZ801

--- scripts/build_hyp_aboot.sh
+++ scripts/build_hyp_aboot.sh
@@ -6,7 +6,7 @@ make -C src/qhypstub CROSS_COMPILE=aarch64-linux-gnu-
 # inititalizing the mmc (maybe due to using old/recycled flash chips)
 echo 'DEFINES += USE_TARGET_HS200_CAPS=1' >> src/lk2nd/project/lk1st-msm8916.mk
 
-make -C src/lk2nd LK1ST_DTB=msm8916-512mb-mtp LK1ST_COMPATIBLE=zhihe,uz801-v3 \
+make -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="yiming,uz801-v3" \
     TOOLCHAIN_PREFIX=arm-none-eabi- lk1st-msm8916
 
 # test sign
